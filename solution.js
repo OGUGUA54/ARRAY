fe274e05fdedf2b3f1d9f0b38d6723ed48cd37c3
OGUGUA54 <nndukaobikwelu@gmail.com>
2022-08-01 16:27:19 +0100

    NEW COMMIT

---
+++ solution.js
@@ -0,0 +1,11 @@
+let cat = {
+    name: 'ogugua',
+    age: '25',
+    schoolName: 'FederalGovernmentCollege'
+  }
+  console.log(cat)
+  cat.job = 'Software development'
+  cat.schoolName = 'FederalGovernmentCollegeEnugu'
+  cat.age = 13
+  console.log(cat) 
+  
